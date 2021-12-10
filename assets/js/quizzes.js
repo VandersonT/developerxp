1b5c3737a1bbb8878d09fa32711c4f13c4c2dab0
VandersonThiago <vandersontpaulo@gmail.com>
2021-12-10 19:10:36 -0300

    Criado a base dos quizzes no js e feito a renderização deles na tela js redirecionando para a tela de quiz aberto com uma seção informando o clicado

---
+++ assets/js/quizzes.js
@@ -0,0 +1,78 @@
+let quizzes = [
+    {
+        id:1,
+        name:'JavaScript',
+        description: 'Teste Agora os seus conhecimentos na linguagem de programação javaScript.',
+        img: 'javaScript.jpg',
+        questions:[
+            {
+                question: 'O que “1” + 2 + 4 retorna?',
+                options: [
+                    '6',
+                    '7',
+                    '124',
+                    '24'
+                ],
+                rightAnswer: 1
+            },
+            {
+                question: 'O que é NaN?',
+                options: [
+                    'Not a Name',
+                    'Not a Number',
+                    'Not a New',
+                    'Nenhuma opção acima'
+                ],
+                rightAnswer: 1
+            },
+            {
+                question: 'Qual seria o resultado de 3 + 2 + ”7 ″?',
+                options: [
+                    '57',
+                    '327',
+                    '12',
+                    '32'
+                ],
+                rightAnswer: 1
+            }
+        ]
+    },
+    {
+        id:2,
+        name:'JavaScript',
+        description: 'Teste Agora os seus conhecimentos na linguagem de programação javaScript.',
+        img: 'javaScript.jpg',
+        questions:[
+            {
+                question: 'O que “1” + 2 + 4 retorna?',
+                options: [
+                    '6',
+                    '7',
+                    '124',
+                    '24'
+                ],
+                rightAnswer: 1
+            },
+            {
+                question: 'O que é NaN?',
+                options: [
+                    'Not a Name',
+                    'Not a Number',
+                    'Not a New',
+                    'Nenhuma opção acima'
+                ],
+                rightAnswer: 1
+            },
+            {
+                question: 'Qual seria o resultado de 3 + 2 + ”7 ″?',
+                options: [
+                    '57',
+                    '327',
+                    '12',
+                    '32'
+                ],
+                rightAnswer: 1
+            }
+        ]
+    },
+];
---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+//downArrow animation
+document.querySelector('.downArrow').addEventListener('click', function(){
+    let height = window.innerHeight;
+    window.scroll({top: height, left: 0, behavior: 'smooth' })
+});
+
+
+//Benefits animatio
+window.addEventListener("scroll", function (event) {
+    let scroll = this.scrollY;
+    let benefitsSingle = document.querySelectorAll('.benefits--single');
+    if(scroll >= 100){
+        
+        for(let i =0; i < benefitsSingle.length; i++){
+            benefitsSingle[i].style.display = 'block';
+        }
+
+    }
+});
+
+//Renderiza os quizzes na tela
+quizzes.map((item, index)=>{
+    let quizSingle = document.querySelector('.quizSingleModel .quizSingle').cloneNode(true);
+
+    quizSingle.querySelector('img').src = "./assets/images/"+item.img;
+    quizSingle.querySelector('h1').innerText = item.name;
+    quizSingle.querySelector('p').innerText = item.description;
+
+    quizSingle.addEventListener('click', ()=>{
+        sessionStorage.setItem('currentQuiz', item.id);
+        window.location.href = "http://127.0.0.1:5500/openQuiz.html";
+    });
+
+    document.querySelector('.quizzes').append(quizSingle);
+});
