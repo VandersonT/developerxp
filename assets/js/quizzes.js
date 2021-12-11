a9b42d0368da84cf2a439eb1cf7e52e336e5fb73
VandersonThiago <vandersontpaulo@gmail.com>
2021-12-11 12:02:45 -0300

    Criado a página do quiz aberto com o responsivo

--- assets/js/quizzes.js
+++ assets/js/quizzes.js
@@ -23,7 +23,7 @@ let quizzes = [
                     'Not a New',
                     'Nenhuma opção acima'
                 ],
-                rightAnswer: 1
+                rightAnswer: 0
             },
             {
                 question: 'Qual seria o resultado de 3 + 2 + ”7 ″?',
@@ -33,43 +33,43 @@ let quizzes = [
                     '12',
                     '32'
                 ],
-                rightAnswer: 1
+                rightAnswer: 0
             }
         ]
     },
     {
         id:2,
-        name:'JavaScript',
-        description: 'Teste Agora os seus conhecimentos na linguagem de programação javaScript.',
-        img: 'javaScript.jpg',
+        name:'Html',
+        description: 'Teste Agora os seus conhecimentos na linguagem de marcação html.',
+        img: 'html.jpg',
         questions:[
             {
-                question: 'O que “1” + 2 + 4 retorna?',
+                question: 'Html é uma linguagem de...',
                 options: [
-                    '6',
-                    '7',
-                    '124',
-                    '24'
+                    'Estilização',
+                    'Programação',
+                    'Exclusão',
+                    'Marcação'
                 ],
-                rightAnswer: 1
+                rightAnswer: 3
             },
             {
-                question: 'O que é NaN?',
+                question: 'Para criar um link entre uma página e outra, usa-se a tag',
                 options: [
-                    'Not a Name',
-                    'Not a Number',
-                    'Not a New',
-                    'Nenhuma opção acima'
+                    '<href a="pagina.html"></a>',
+                    '<a="pagina.html"></a>',
+                    '<a href="pagina.html"></a>',
+                    '<a href=pagina_html></a>'
                 ],
-                rightAnswer: 1
+                rightAnswer: 2
             },
             {
-                question: 'Qual seria o resultado de 3 + 2 + ”7 ″?',
+                question: 'Como deve ser declarado um título da página?',
                 options: [
-                    '57',
-                    '327',
-                    '12',
-                    '32'
+                    '<title title="titulo">',
+                    '<title>Título aqui</title>',
+                    '<html title="Título aqui"></html>',
+                    '<pageTitle>Titulo</pageTitle>'
                 ],
                 rightAnswer: 1
             }
